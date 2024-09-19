97083d01dd52576a6eeebe4fd41eae3b2b7e9d11
katarsi <katarsi@docker.devosfernando.com>
2024-09-19 18:08:04 -0500

    Actualizacion de dispositivo y estructura de scaneo

--- main.py
+++ main.py
@@ -1,22 +1,32 @@
-
-import subprocess, json, time, os, requests
+import subprocess, json, time, os, requests, sys, socket, ipaddress
 from scapy.all import ARP, Ether, srp
 from mac_vendor_lookup import MacLookup
 
 # Función para enviar push de alerta
-def enviar_mensaje(message):
-    url = 'http://192.168.192.201:6367/send_message'  # Cambia el URL si es necesario
+def enviar_mensaje(url_mensajes, equipos, lugar):
     headers = {'Content-Type': 'application/json'}
-    data = {'message': message}
+    if equipos['estado'] == "offline":
+        data = {'message': f"{lugar} - {equipos['mac']} ({equipos['nombre']}) 📴 ha dejado de estar en línea"}
+    else:
+        data = {'message': f"{lugar} - {equipos['mac']} ({equipos['nombre']}) 📶 ha vuelto a estar en línea"}
     
     try:
-        response = requests.post(url, json=data, headers=headers)
+        response = requests.post(url_mensajes, json=data, headers=headers)
         if response.status_code == 200:
             print("Mensaje enviado con éxito")
         else:
             print(f"Error al enviar mensaje: {response.status_code}, {response.text}")
     except Exception as e:
         print(f"Error al conectar con el servidor: {e}")
+
+# Indica si la ip esta en el rango de la red
+def ip_en_rango(ip_str, rango_str):
+    # Convertir las cadenas en objetos de IP y red
+    ip = ipaddress.ip_address(ip_str)
+    rango = ipaddress.ip_network(rango_str, strict=False)
+    
+    # Verificar si la IP está en el rango
+    return ip in rango
 
 # Función para crear un paquete ARP
 def create_arp_packet(ip_range):
@@ -26,7 +36,7 @@ def create_arp_packet(ip_range):
     return broadcast / arp_request
 
 # Función para hacer ping a una IP con reintentos
-def ping(ip, max_retries=3, timeout=1):
+def ping(ip, max_retries=9, timeout=1):
     for _ in range(max_retries):
         # Ejecuta el comando ping con un timeout
         result = subprocess.run(['ping', '-c', '1', '-W', str(timeout), ip], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -36,6 +46,7 @@ def ping(ip, max_retries=3, timeout=1):
 
 # Función para escanear la red y actualizar la lista de dispositivos
 def scan_network(ip_range, equipos):
+    print(f"------------> Escaneando la red ...")
     # Envía el paquete y recibe respuestas
     packet = create_arp_packet(ip_range)
     try:
@@ -49,7 +60,7 @@ def scan_network(ip_range, equipos):
     mac_lookup = MacLookup()
     # Iterar sobre la lista de dispositivos respondidos
     for element in answered_list:
-        print(element[1].hwsrc, element[1].psrc)
+        print(f'------------> {element[1].hwsrc} - {element[1].psrc}')
         mac_address = element[1].hwsrc
         ip_address = element[1].psrc
         try:
@@ -69,18 +80,122 @@ def scan_network(ip_range, equipos):
                 'reintentos': 0,
                 'alertar': True,
             }
+        else:
+            # Si la MAC ya existe, solo actualizar la IP y el estado
+            if equipos[mac_address]['ip'] != ip_address:
+                equipos[mac_address]['ip'] = ip_address
+                equipos[mac_address]['estado'] = 'online'
+                equipos[mac_address]['reintentos'] = 0  # Reiniciar el contador de reintentos si está online
+    print(f"------------> Escaneando la red ...")
     return equipos 
 
+# Función para obtener la dirección MAC de un dispositivo
+def get_mac(ip):
+    # Construir un paquete ARP
+    arp_request = ARP(pdst=ip)
+    ether = Ether(dst="ff:ff:ff:ff:ff:ff")
+    packet = ether/arp_request
+
+    # Enviar el paquete ARP y capturar la respuesta
+    result = srp(packet, timeout=3, verbose=0)[0]
+
+    # Si hay una respuesta, devuelve la dirección MAC
+    if result:
+        return result[0][1].hwsrc
+    else:
+        return None
+
+# Función para obtener todas las IPs en una subred
+def get_all_ips_in_subnet(subnet):
+    # Crear un objeto IPv4Network a partir del rango de subred
+    network = ipaddress.ip_network(subnet)
+    
+    # Retornar todas las IPs en la subred (exceptuando la red y el broadcast)
+    return [str(ip) for ip in network.hosts()]
+
+# Función para escanear la red y actualizar la lista de dispositivos
+def scanGlobal(equipos, ip_range):
+    # Control de dispositivos
+    mac_lookup = MacLookup()
+    list = get_all_ips_in_subnet(ip_range)
+    for ip in list:
+        print(f'\r -> Revisando  {ip}              ', end='', flush=True)
+        if ping(ip,1,1):
+            mac = get_mac(ip)
+            try:
+                # Intenta obtener el fabricante usando la dirección MAC
+                vendor = mac_lookup.lookup(mac)
+            except Exception:
+                vendor = "Unknown"
+            # Si responde al ping, agregarlo o actualizarlo en la lista
+            if mac in equipos:
+                print(f'\r -> Revisando {ip}, ACTUALIZANDO\n')
+                # Actualizar la información del dispositivo
+                equipos[mac].update({
+                    'ip': ip,
+                    'vendor': vendor,
+                    'estado': 'online',  # Actualizamos el estado a 'online'
+                    'reintentos': 0,     # Reiniciamos el contador de reintentos
+                })
+            else:
+                # Si no existe, agregar el dispositivo al diccionario
+                if mac != None:
+                    print(f'\r -> Revisando {ip}, NUEVO\n')
+                    equipos[mac] = {
+                        'ip': ip,
+                        'mac': mac,
+                        'vendor': vendor,
+                        'nombre': 'Desconocido',
+                        'estado': 'online',
+                        'reintentos': 0,
+                        'alertar': True,
+                    }
+    print(f'\r Todo Actualizado                ', end='', flush=True)
+    print(f'')
+    return equipos
+    
 # Función para inicializar el programa
 def inicio():
-    if os.getenv('SCAN_WIFI_LUGAR') == None:
-        lugar = "🛜 - "
-    else:
-        lugar = "🛜 - " + os.getenv('SCAN_WIFI_LUGAR')
-    if os.getenv('SCAN_WIFI_RANGO') == None:
-        ip_range = "192.168.0.1/24"
-    else:
-        ip_range = os.getenv('SCAN_WIFI_RANGO')
+    # Función para identificar la red en la que se encuentra el equipo
+    def identificadorRed():
+        # Lista de redes con sus rangos
+        redes = [
+            {"lugar": "6411 ", "rango": "192.168.80.0/24"},
+            {"lugar": "1105 ", "rango": "192.168.0.0/24" }
+        ]
+        # Función para obtener la IP de la interfaz activa del equipo
+        def obtener_ip_local():
+            s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            try:
+                # No es necesario que la conexión ocurra realmente
+                s.connect(("8.8.8.8", 80))
+                ip_local = s.getsockname()[0]
+            except Exception as e:
+                print(f"Error al obtener la IP local: {e}")
+                ip_local = None
+            finally:
+                s.close()
+            return ip_local
+        # Función para determinar a qué red pertenece la IP del equipo
+        def determinar_red(ip, redes):
+            ip_obj = ipaddress.ip_address(ip)
+            for red in redes:
+                red_obj = ipaddress.ip_network(red["rango"])
+                if ip_obj in red_obj:
+                    return red["lugar"], red["rango"]
+            return None, None
+        # Obtener la IP del equipo
+        ip_local = obtener_ip_local()
+        if ip_local:
+            lugar, rango =  determinar_red(ip_local, redes)
+            if lugar:
+                return lugar, rango
+        return None, None
+    # Obtener las variables de entorno
+    lugar, ip_range = identificadorRed()
+    # Mensaje de alerta
+    url_mensajes = os.getenv('SCAN_WIFI_IP_MENSAJE')
+    # Intentar cargar el archivo de dispositivos, crear uno vacío si no existe
     try:
         # Cargar archivo de dispositivos
         with open('dispositivos.json', 'r') as archivo:
@@ -90,57 +205,51 @@ def inicio():
         with open('dispositivos.json', 'w') as archivo:
             json.dump({}, archivo)
         equipos = {}
-    return lugar, equipos, ip_range
+    # En caso de algun None, se considera error en las variables de entorno
+    if any(var is None for var in [lugar, ip_range, url_mensajes]):
+        print("Error: Se detectó un problema con las variables de entorno. El programa se detendrá.")
+        print(f'export SCAN_WIFI_IP_MENSAJE="http://192.168.192.201:6367/send_message"')
+        sys.exit()  # Salida controlada del programa
+    return "🛜 - " + lugar, ip_range, equipos, url_mensajes
 
 # Función principal
 def main():
     # Inicializar variables
-    lugar, equipos, ip_range = inicio()
-    print(f"------------> Escaneando la red {ip_range}...")
+    lugar, ip_range, equipos, url_mensajes = inicio()
     # Inicializar variables de tiempo
     time_saveDevices= 1
     time_broadcast = 6
     time_ping = 1
     # Escanear la red
-    scan_network(ip_range, equipos)
+    equipos = scan_network(ip_range, equipos)
     # Iniciar el ciclo de monitoreo
+    print("Escaneo forzado de la red")
+    equipos= scanGlobal(equipos, ip_range)
     while True:
-        print(f"Escaneando la red {ip_range}...")
         # 'equipos' es el diccionario que contiene las MAC como claves
-        for equipo in equipos:
-
-            estado = ping(equipos[equipo]['ip'])
-            if estado:
-                if equipos[equipo]['alertar']:
-                    if equipos[equipo]['reintentos'] > 6 and equipos[equipo]['estado'] == 'offline':
-                        print(f"{lugar}📶 - {equipos[equipo]['mac']} ({equipos[equipo]['ip']}) ha vuelto a estar en línea")
-                        enviar_mensaje(f"{lugar}📶 - {equipos[equipo]['mac']} ({equipos[equipo]['ip']}) ha vuelto a estar en línea")
-                        equipos[equipo]['estado'] = 'online'
-                    else:
-                        if equipos[equipo]['reintentos'] < 9:
-                            equipos[equipo]['reintentos'] += 1
-                else:
+        for equipo in equipos:    
+            if ip_en_rango(equipos[equipo]['ip'], ip_range):
+                print(f"{equipos[equipo]['mac']} - {equipos[equipo]['ip'].ljust(15)}  - {equipos[equipo]['nombre'].ljust(40)}  - {equipos[equipo]['estado'].ljust(9)} - {equipos[equipo]['reintentos']}")
+                estado = ping(equipos[equipo]['ip'],1,1)
+                # Verificar si el estado ha cambiado
+                if estado:
+                    #OnLine
                     if equipos[equipo]['reintentos'] < 9:
                         equipos[equipo]['reintentos'] += 1
-                    equipos[equipo]['estado'] = 'online'
-            else:
-                if equipos[equipo]['alertar']:
-                    if equipos[equipo]['reintentos'] > 6 and equipos[equipo]['estado'] == 'online':
-                        print(f"{lugar}📴 - {equipos[equipo]['mac']} ({equipos[equipo]['ip']}) ha dejado de estar en línea")
-                        enviar_mensaje(f"{lugar}📴 - {equipos[equipo]['mac']} ({equipos[equipo]['ip']}) ha dejado de estar en línea")
-                        equipos[equipo]['estado'] = 'offline'
-                    else:
-                        if equipos[equipo]['reintentos'] < 9:
-                            equipos[equipo]['reintentos'] += 1
                 else:
-                    if equipos[equipo]['reintentos'] < 9:
-                        equipos[equipo]['reintentos'] += 1
-                    equipos[equipo]['estado'] = 'offline'
-            print(f"{equipos[equipo]['mac']} ({equipos[equipo]['ip']}) - {equipos[equipo]['estado']} - {equipos[equipo]['reintentos']}")
+                    #OffLine
+                    if equipos[equipo]['reintentos'] >= 0:
+                        equipos[equipo]['reintentos'] -= 1
+                # Incremental para avisos
+                if (equipos[equipo]['reintentos'] == 9 or equipos[equipo]['reintentos'] == 0)and equipos[equipo]['alertar']:
+                    print(f"{lugar}📶 - {equipos[equipo]['mac']} ({equipos[equipo]['nombre'].ljust(40)}) Cambia estado")
+                    enviar_mensaje(url_mensajes, equipos[equipo], lugar)
+        print("----------------------------------------------------------------------------------------------------")
+        time.sleep(9)
         # Esperar un tiempo antes de volver a escanear
         time.sleep(time_ping)
         if time_broadcast <= 0:
-            scan_network(ip_range, equipos)
+            equipos = scan_network(ip_range, equipos)
             time_broadcast = 6
             if time_saveDevices <= 0:
                 with open('dispositivos.json', 'w') as archivo:
@@ -154,4 +263,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-        
